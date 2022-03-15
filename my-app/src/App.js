74e3dadade31120c51d2587608fd23584c2fb358
Opaul Chanlongsawettakun <Opaul.chan@gmail.com>
2022-03-15 20:50:20 +0700

    react hook increment button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,29 +1,48 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react';
 
-export default class App extends Component {
 
-  constructor(props) {
-    super(props);
+export default function App() {
 
-    this.state ={
-      buttonText: 'Click me please!'
-    }
+  const [buttonText, setButtonText] = useState("Click me,please");
+  const [increment, setIncrement] = useState(0);
 
-    this.handleClick = this.handleClick.bind(this);
+  return (
+    <div>
+  <button onClick={() => setButtonText("Thanks, been clicked!")}>{buttonText}</button>
 
-  }
-
-  handleClick(){
-    this.setState(() => {
-      return {buttonText:'Thanks, been clicked!'}
-    });
-  }
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleClick}>{this.state.buttonText}</button>
-      </div>
-    )
-  }
+  <h1>{increment}</h1>
+  <button onClick={() => setIncrement(increment +1)}>Click to increment</button>
+  <button onClick={() => setIncrement(increment -1)}>Click to decrement</button>  
+      
+    </div>
+  )
 }
+
+
+// export default class App extends Component {
+
+//   constructor(props) {
+//     super(props);
+
+//     this.state ={
+//       buttonText: 'Click me please!'
+//     }
+
+//     this.handleClick = this.handleClick.bind(this);
+
+//   }
+
+//   handleClick(){
+//     this.setState(() => {
+//       return {buttonText:'Thanks, been clicked!'}
+//     });
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.handleClick}>{this.state.buttonText}</button>
+//       </div>
+//     )
+//   }
+// }
 
